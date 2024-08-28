5c24a6962ecdd99c6cde6794347c99ef303d9109
jeremy.kang'ethe@strathmore.edu <jeremy.kangethe@gmail.com>
2024-08-28 15:00:26 +0300

    First php commit

---
+++ fnc.php
@@ -0,0 +1,14 @@
+<?php
+
+class fnc{
+     var $fname;
+     public $username;
+     protected $email_address;
+     private $password;
+
+     public function computer_user($fname){
+        return $fname;
+     }
+
+}
+?>
